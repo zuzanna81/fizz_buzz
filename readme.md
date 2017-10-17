604ba22b018b152d1d3079c12ceb86b18975548d
Zuzanna <zuzanna.bienkowska@gmail.com>
2017-10-17 21:02:03 +0200

    Initial commit

---
+++ readme.md
@@ -0,0 +1,9 @@
+#Fizz_Buzz
+
+##It's all about the test :-)
+
+* First object
+* Second object
+
+1. First sub-object
+2. Second sub-object
---
+++ spec/fizz_buzz_spec.rb
@@ -0,0 +1,9 @@
+require './lib/fizz_buzz'
+
+describe 'fizz_buzz' do
+
+end
+
+it 'returns 1 if number is 1' do
+    expect(fizz_buzz(1)).to eq 1
+end
