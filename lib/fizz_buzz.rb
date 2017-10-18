6cece8154e39d0056b78fd869cf0b71e2c293826
Zuzanna <zuzanna.bienkowska@gmail.com>
2017-10-18 20:17:06 +0200

    Committing fizz_buzz

--- lib/fizz_buzz.rb
+++ lib/fizz_buzz.rb
@@ -1,3 +1,17 @@
 def fizz_buzz(number)
-  number
+  if has_zero_remainder?(number, 15)
+    'fizz buzz'
+  elsif has_zero_remainder?(number, 5)
+    'buzz'
+  elsif has_zero_remainder?(number, 3)
+    'fizz'
+  elsif number < 0
+    'No negative numbers please'
+  else
+    number
+  end
+end
+
+def has_zero_remainder?(number, divider)
+  number % divider == 0
 end
--- spec/fizz_buzz_spec.rb
+++ spec/fizz_buzz_spec.rb
@@ -5,4 +5,21 @@ describe 'fizz_buzz' do
   it 'returns 1 if number is 1' do
     expect(fizz_buzz(1)).to eq 1
   end
+
+  it "returns 'fizz' if number is divisible by 3" do
+    expect(fizz_buzz(3)).to eq 'fizz'
+  end
+
+  it "returns 'buzz' if number is divisible by 5" do
+    expect(fizz_buzz(5)).to eq 'buzz'
+  end
+
+  it "returns 'fizz buzz' if number is divisible by 15" do
+    expect(fizz_buzz(15)).to eq 'fizz buzz'
+  end
+
+  it "returns 'No negative numbers please' if number is negative" do
+    expect(fizz_buzz(-1)).to eq 'No negative numbers please'
+  end
+
 end
